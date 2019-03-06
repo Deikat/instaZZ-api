5bb08c3178da416f7ae179dfd420fd19cab0161f
Mathilde Henry <mathilde.henry@etu.uca.fr>
2019-03-06 20:34:46 +0100

    change port choice

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ db.once("open", function() {
 
   app.use("/", Routes);
 
-  const PORT = 8080;
+  const PORT = process.env.PORT || 5000;
 
   app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
